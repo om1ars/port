5f236904ef5d9ed997a3a6c851e92d92a7aedbd3
omar-slf <nurullayevumarali1@gmail.com>
2021-11-29 14:31:46 +0500

    Firebase

---
+++ .history/src/firebase_20211129143039.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyClizZTF6DDYN-PXl4_7T82RbV5BMHRHf0",
+  authDomain: "portfolio--omar.firebaseapp.com",
+  projectId: "portfolio--omar",
+  storageBucket: "portfolio--omar.appspot.com",
+  messagingSenderId: "197070042135",
+  appId: "1:197070042135:web:4df0536d2e60f9a5b5d852"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
---
+++ .history/src/firebase_20211129143043.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyClizZTF6DDYN-PXl4_7T82RbV5BMHRHf0",
+  authDomain: "portfolio--omar.firebaseapp.com",
+  projectId: "portfolio--omar",
+  storageBucket: "portfolio--omar.appspot.com",
+  messagingSenderId: "197070042135",
+  appId: "1:197070042135:web:4df0536d2e60f9a5b5d852"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
